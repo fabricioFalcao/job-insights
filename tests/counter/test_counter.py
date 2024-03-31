d9ca16f0ae7db0e4c398cd875240604d6c1d8850
fabricioFalcao <fabriciohfd@gmail.com>
2024-03-31 17:39:04 -0300

    Requisito 1 novamente

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,4 +2,5 @@ from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    assert count_ocurrences("../../data/jobs.csv", "python") == 1639
+    assert count_ocurrences("data/jobs.csv", "python") == 1639
+    assert count_ocurrences("data/jobs.csv", "Javascript") == 122
