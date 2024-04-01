29c0ac43444d8d48467ce0d286c2c55f19de0a10
fabricioFalcao <fabriciohfd@gmail.com>
2024-04-01 10:54:54 -0300

    Requisito 10

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,11 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    raise
+    translated_jobs = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+
+    assert translated_jobs[0] == {
+        "title": "Maquinista",
+        "salary": "2000",
+        "type": "trainee",
+    }
